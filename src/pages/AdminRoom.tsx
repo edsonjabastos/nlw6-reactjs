ade8605d1b9fde6d91b3f74612ae2c69a1279afb
edsonjabastos <edsonjabastos@hotmail.com>
2021-07-04 13:02:01 -0300

    aula 5 finish

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -8,6 +8,8 @@ import { RoomCode } from '../components/RoomCode';
 import '../styles/room.scss'
 import logoImg from '../assets/images/logo.svg';
 import deleteImg from '../assets/images/delete.svg';
+import checkImg from '../assets/images/check.svg';
+import answerImg from '../assets/images/answer.svg';
 
 import { Question } from '../components/Question';
 import { useRoom } from '../hooks/useRoom';
@@ -31,7 +33,7 @@ export function AdminRoom() {
   const roomId = params.id;
   const { title, questions } = useRoom(roomId);
 
-  async function handleEndRoom(){
+  async function handleEndRoom() {
     database.ref(`rooms/${roomId}`).update({
       endedAt: new Date(),
     })
@@ -39,10 +41,23 @@ export function AdminRoom() {
     history.push('/')
   }
 
-   async function handleDeleteQuestion(questionId: string) {
-    if (window.confirm('Tem certeza que você deseja deletar esta pergunta?')){
+  async function handleDeleteQuestion(questionId: string) {
+    if (window.confirm('Tem certeza que você deseja deletar esta pergunta?')) {
       await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
     }
+  }
+  async function handleCheckQuestionAsAnswered(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isAnswered: true,
+    });
+
+  }
+
+  async function handleHighLightQuestion(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isHighlighted: true,
+    });
+
   }
 
   return (
@@ -62,9 +77,6 @@ export function AdminRoom() {
           <h1>Sala {title}</h1>
           {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
         </div>
-
-
-
         <div className="question-list">
           {questions.map(question => {
             return (
@@ -72,13 +84,32 @@ export function AdminRoom() {
                 key={question.id} //ALGORITMO DE RECONCILIAÇÃO
                 content={question.content}
                 author={question.author}
+                isAnswered={question.isAnswered}
+                isHighlighted={question.isHighlighted}
               >
+                {!question.isAnswered && (
+                  <>
+                    <button
+                      type="button"
+                      onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                    >
+                      <img src={checkImg} alt="Marcar pergunta como respondida." />
+                    </button>
+                    <button
+                      type="button"
+                      onClick={() => handleHighLightQuestion(question.id)}
+                    >
+                      <img src={answerImg} alt="Dar destaque à pergunta." />
+                    </button>
+                  </>
+                )}
                 <button
                   type="button"
                   onClick={() => handleDeleteQuestion(question.id)}
                 >
                   <img src={deleteImg} alt="Remover pergunta" />
                 </button>
+
               </Question>
             )
           })}
